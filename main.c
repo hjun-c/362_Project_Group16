b2eb905a528c0cd77fb1646d2cfe0a77fa496f07
hjun-c <116941691+hjun-c@users.noreply.github.com>
2024-10-20 02:24:48 -0400

    outline

--- main.c
+++ main.c
@@ -9,69 +9,140 @@
 
 // Include librarires
 #include "stm32f0xx.h"
+#include "ff.h"
 
 // Define constants
+#define RATE 20000
+#define DEBOUNCE_TIME 50
 
 // Declare global variables
+FIL file;
+DIR Dir;
+FATFS fatfs;
+FRESULT fr;
+UINT bytesRead;
+int button_state = 0;
+int debounce_counter = 0;
 
+// Function Declaration
+void internal_clock();
+//==============================
+void init_buttons(void);
+void scan_buttons(void);
+void init_tim7(void);
+void TIM7_IRQHandler(void);
+//==============================
+void init_DAC(void);
+void init_tim6(void);
+void TIM6_DAC_IRQHandler(void);
+void play_song(char* song_title);
+void stop_song(void);
+//==============================
+void init_spi1_slow(void);
+void enable_sdcard(void);
+void disable_sdcard(void);
+void init_sdcard_io(void);
+void sdcard_io_high_speed(void);
+void init_sd_card(void);
+//==============================
+void init_lcd_spi(void);
+void handle_button1(void);
+void handle_button2(void);
+void handle_button3(void);
+void handle_button4(void);
+void display_song_list(void);
+void display_playing_song(void);
+void display_song_stopped(void);
+//==============================
+int open_file(char filename[]);
+void read_file(void);
 
-/*
-Function lists
-1. System Initialization
-  - initialize peripherals (o)
-  - initialize gpio (o)
-  - initialize internal clock
-2. Access SD card
-  - initialize spi1 (communicate with SD card) (o)
-  - enable card (o)
-  - disable card (o)
-3. Handle WAV file
-  - open
-  - close
-  - read
-4. DAC
-  - initialize DAC (o)
-  - configure DAC pins (o)
-  - start song
-  - stop song
-5. Timer
-  - initialize TIM6 (for DAC)
-  - TIM6 interrupt handler
-  - initialize TIM3 (for button)
-  - TIM3 interrupt handler
-6. TFT Display
-  - configure pins for display (o)
-  - initialize TFT display
-  - display song title
-  - display playing mode
-  - display stop mode
-7. Button
-  - configure buttons (o)
-  - read state of click button
-  - read state of direction button
-  - read state of home button
-  - button handler
-8.
-*/
-
-// How to read file?
-
-
+// structure of WAV file header (for storing information)
+struct WavFileHeader {
+  char ck_id[4]; // chunk id
+  int ck_size; // chunk size
+  char sub_ck_id[4]; // sub chunk 1 id
+  int sub_ck_size; // sub chuck 1 size
+  int audio_format;
+  int num_channels;
+  int sample_rate;
+  int byte_rate;
+  int block_align;
+  int bits_per_sample;
+  char sub_ck2_id[4]; // sub chunk 2 id
+  int sub_ck2_size; // sub chunk 2 size
+  int data_size;
+} header;
 
 int main(void) {
-
+  internal_clock();
 }
 
-void init_button(void) {
+void init_buttons(void) {
   // Enable the RCC clock for Port C
   RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 
-  // Configure pins PC0 – PC3 to be inputs
+  // Configure pins PC0 – PC3 as inputs
+  // Button 1: up
+  // Button 2: down
+  // Button 3: play or stop
+  // Button 4: back to song list
   GPIOC->MODER &= ~((0x3) | (0x3 << 2) | (0x3 << 4) | (0x3 << 6)); // input mode
 
-  // Configure pins PC0 – PC3 to be internally pulled low
+  // Configure pins PC0 – PC3 to be internally pulled down
+  // (Default state will be floating if no button pressed)
   GPIOC->PUPDR &= ~((0x3 << 0) | (0x3 << 2) | (0x3 << 4) | (0x3 << 6)); // clear
   GPIOC->PUPDR |= (0x2) | (0x2 << 2) | (0x2 << 4) | (0x2 << 6); // set to be pulled down
+}
+
+void scan_buttons(void) {
+  if (debounce_counter > 0) {
+    debounce_counter--;
+    return;
+  }
+
+  // Check which button was pressed
+  int new_button_state = (GPIOC->IDR & 0xF);
+
+  if (button_state != new_button_state) {
+    button_state = new_button_state;
+    debounce_counter = DEBOUNCE_TIME;
+
+    if (button_state & 0x1) {
+      // move cursor up
+    }
+    if (button_state & 0x2) {
+      // move cursor down
+    }
+    if (button_state & 0x8) {
+      // select
+    }
+    if (button_state & 0xF) {
+      // back to song list
+    }
+  }
+}
+
+void init_tim7(void) {
+  // Enable the RCC clock for TIM7
+  RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;
+
+  // Set the Prescaler (PSC) and Auto-Reload Register (ARR)
+  TIM7->PSC = 480-1;
+  TIM7->ARR = 100-1;
+
+  // Enable the UIE bit in the DIER to enable the UIE flag
+  TIM7->DIER |= TIM_DIER_UIE;
+
+  // Enable the interrupt for Timer 7 in the NVIC ISER
+  NVIC_EnableIRQ(TIM7_IRQn);
+
+  // Enable Timer 7 by setting the CEN bit in the Timer 7 Control Register 1
+  TIM7->CR1 |= TIM_CR1_CEN;
+}
+
+void TIM7_IRQHandler(void) {
+  scan_buttons();
 }
 
 void init_DAC(void) {
@@ -92,6 +163,52 @@ void init_DAC(void) {
 
   // Enable the DAC
   DAC->CR |= 0x1;
+}
+
+void init_tim6(void) {
+  // Enable the RCC clock for TIm6
+  RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+
+  // Set the prescaler and reload value
+  TIM6->PSC = 100 - 1;
+  TIM6->ARR = (48000000 / (RATE * (TIM6->ARR + 1))) - 1;
+
+  // Set the value that enables a TRGO on an Update event
+  TIM6->CR2 &= ~((0x3 << 4) | (0x3 << 5) | (0x3 << 6)); // clear
+  TIM6->CR2 |= (0x1 << 5);
+
+  // Enable the UIE bit in the DIER to enable the UIE flag
+  TIM6->DIER |= TIM_DIER_UIE;
+
+  // Enable the interrupt for the Timer 6
+  NVIC_EnableIRQ(TIM6_DAC_IRQn);
+
+  // Enable the Timer 6
+  TIM6->CR1 |= TIM_CR1_CEN;
+}
+
+void TIM6_DAC_IRQHandler(void) {
+  // Acknowledge the interrupt
+  if (TIM6->SR & TIM_SR_UIF) {
+    TIM6->SR &= ~TIM_SR_UIF; // clear the update interrupt flag
+  }
+
+  // Output the next sample to the DAC
+  int samp;
+
+  // Send data to DAC->DHR12R1
+  DAC->DHR12R1 = samp;
+}
+
+void play_song(char* song_title) {
+  open_file(song_title);
+  if (fr == FR_OK) {
+    read_file();
+  }
+}
+
+void stop_song(void) {
+
 }
 
 void init_spi1_slow(void) {
@@ -153,6 +270,10 @@ void sdcard_io_high_speed(void) {
   SPI1->CR1 |= SPI_CR1_SPE;
 }
 
+void init_sd_card(void) {
+  f_mount(&fatfs, "", 0);
+}
+
 void init_lcd_spi(void) {
   // Enable the RCC clock for Port B
   RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
@@ -163,4 +284,40 @@ void init_lcd_spi(void) {
 
   init_spi1_slow();
   sdcard_io_high_speed();
+}
+
+void handle_button1(void) {
+  spi_cmd(); // move the cursor up
+}
+
+void handle_button2(void) {
+  spi_cmd(); // move the cursor down
+}
+
+void handle_button3(void) {
+  spi_cmd(); // move the cursor to the home position
+}
+
+void handle_button4(void) {
+  // cursor doesn't needed
+}
+
+void display_song_list(void) {
+
+}
+
+void display_playing_song(void) {
+
+}
+
+void display_song_stopped(void) {
+
+}
+
+int open_file(char filename[]) {
+  fr = f_open(&file, filename, FA_READ);
+}
+
+void read_file(void) {
+  f_read(&file, &header, sizeof header, &bytesRead);
 }
